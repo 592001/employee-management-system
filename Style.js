6d06bfbdba4c6ee43f589de2e153cc82641aeb92
Santhosh <136967908+592001@users.noreply.github.com>
2026-01-21 00:03:58 +0530

    Create Style.js

---
+++ Style.js
@@ -0,0 +1,23 @@
+body {
+  font-family: Arial;
+  background: #f2f2f2;
+}
+
+.container {
+  width: 400px;
+  margin: 40px auto;
+  background: white;
+  padding: 20px;
+  border-radius: 6px;
+}
+
+input, button {
+  width: 100%;
+  margin: 5px 0;
+  padding: 8px;
+}
+
+table {
+  width: 100%;
+  margin-top: 10px;
+}
